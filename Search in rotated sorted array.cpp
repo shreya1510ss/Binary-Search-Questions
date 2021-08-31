b23ed1f88ebfa738a4379fe8b5c0e943eb6b74ba
shreya1510ss <84861803+shreya1510ss@users.noreply.github.com>
2021-08-31 23:17:45 +0530

    Update Search in rotated sorted array.cpp

--- Search in rotated sorted array.cpp
+++ Search in rotated sorted array.cpp
@@ -2,86 +2,67 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) 
     {
-        int pivot_index;
+        int pivot=nums.size()-1;
         int low=0;
         int high=nums.size()-1;
         int mid;
-        int start=nums[0];
         
-        if(nums.size()==1)
-        { 
-            if(nums[0]==target)
-                return 0;
-            else 
-                return -1;
-        }
         
-        else if(nums[0]<nums[nums.size()-1])
-        {
-            pivot_index=nums.size()-1;
-            
-            
-        }
-        
-        else
-        {
+        //finding the pivot element
         while(low<=high)
         {
             mid=low+(high-low)/2;
             
-            if(nums[mid]>nums[mid+1])
+            if( mid<(nums.size()-1) && nums[mid]>nums[mid+1])
             {
-                pivot_index=mid;
+                pivot=mid;
                 break;
             }
             
-            else if(nums[mid]<nums[mid-1])
+            
+            else if(mid>0 && nums[mid]<nums[mid-1])
             {
-                pivot_index=mid-1;
+                pivot=mid-1;
                 break;
             }
             
-            else if(nums[mid]<start)
+            else if(nums[mid]<nums[0])
                 high=mid-1;
-            
-            else if(nums[mid]>=start)
+            else if(nums[mid]>=nums[0])
                 low=mid+1;
-            
-
-        }
-            
         }
         
-        if(target<start)
+        
+        
+        //initialising low and high
+        if(target>=nums[0])
         {
-            low=pivot_index+1;
+            low=0;
+            high=pivot;
+        }
+        
+        else if(target<nums[0])
+        {
+            low=pivot+1;
             high=nums.size()-1;
         }
         
-        else
-        {
-            low=0;
-            high=pivot_index;
-        }
-        
-        
-        
+
+        // searching for the tagret element
         while(low<=high)
         {
             mid=low+(high-low)/2;
             
             if(nums[mid]==target)
                 return mid;
-            
-            else if(nums[mid]>target)
-                high=mid-1;
-            
-            else
+            else if(nums[mid]<target)
                 low=mid+1;
+            else 
+                high=mid-1;
         }
-        
-        
-        return -1;
+
+                return -1;
         
     }
+        
 };
